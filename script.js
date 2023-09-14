8e41f4f031ebfe2eb7bccc91fd772dbc63355b3f
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-14 12:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 let count=0;
 const byteSize = (str) => {
   // write your code here
-	for(let i=0; i<str.length; i++){
-		count++;
-	}
-	if(str==""){
-		count=0;
-	}
-	return count;
+	const bytes = new Blob([str]);
+  return bytes.size;
+
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+alert(byteSize(str))
